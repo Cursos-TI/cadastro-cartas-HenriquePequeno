087cf277b6623e34f5b5e3bfa302a817a6c5ac99
Henrique Pequeno <henrickpequeno@gmail.com>
2025-09-16 00:13:44 +0000

    Adiciona cadastro e exibição da segunda carta no jogo Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,11 +9,11 @@ int main() {
   int populacao1, populacao2, turistico1, turistico2;
   float area_km1, area_km2, pib1, pib2;
   char estado1, estado2;
-  char cod_carta1[5], cod_carta2[5];
-  char nome_cidade1[30], nome_cidade2[30];
+  char cod_carta1[5], cod_carta2[5], nome_cidade1[30], nome_cidade2[30];
 
   // Área para entrada de dados
 
+    printf("Dados da primeira carta\n");  
     printf("Informe o nome do Estado\n");
     printf("(O estado deve ser uma letra entra 'A' a 'H'): ");
     scanf("%c", &estado1);
@@ -35,7 +35,32 @@ int main() {
     scanf("%f", &pib1);
 
     printf("Informe o número de pontos turísticos: ");
-    scanf("%d", &turistico1);
+    scanf("%d\n", &turistico1);
+
+
+    printf("Dados da segunda carta\n");
+    printf("Informe o nome do Estado\n");
+    printf("(O estado deve ser uma letra entra 'A' a 'H'): ");
+    scanf("%c", &estado2);
+
+    printf("Informe o código da Carta\n");
+    printf("(O código deve ser um número entra '01' a '04'): ");
+    scanf("%s", cod_carta2);
+
+    printf("Informe o nome da cidade: ");
+    scanf("%s", nome_cidade2);
+
+    printf("Informe o número da população: ");
+    scanf("%d", &populacao2);
+
+    printf("Informe a área em Km²: ");
+    scanf("%f", &area_km2);
+
+    printf("Informe o PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Informe o número de pontos turísticos: ");
+    scanf("%d", &turistico2);
 
   // Área para exibição dos dados da cidade
 
@@ -54,5 +79,21 @@ int main() {
 
     printf("Número de pontos turísticos: %d\n", turistico1);
 
+
+    printf("\n\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+
+    printf("Código da Carta: %c%s\n",estado2, cod_carta2);
+
+    printf("Nome da cidade: %s\n", nome_cidade2);
+
+    printf("População: %d\n", populacao2);
+
+    printf("Área: %f Km²\n", area_km2);
+
+    printf("PIB: %f\n", pib2);
+
+    printf("Número de pontos turísticos: %d\n", turistico2);
+
 return 0;
-} 
+}
